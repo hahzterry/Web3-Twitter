e508bfdcac3c2fa816d935575d07e275bb493bbd
Wizard of Hahz <info@blkluv.org>
2026-09-18 16:13:33 -0400

    Refactor page.tsx to use useActiveAccount

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,21 +1,23 @@
-import React from 'react'
-import { ConnectButton } from 'thirdweb/react'
-import { chain } from './chain'
-import { client } from './client'
-import UserStatus from '../../components/UserStatus'
-import { Twitter } from 'lucide-react'
-import Footer from '../../components/Footer'
+"use client";
+import { ConnectButton, useActiveAccount } from "thirdweb/react";
+import { chain } from "./chain";
+import { client } from "./client";
+import UserStatus from "@/components/UserStatus";
+import { Twitter } from "lucide-react";
+import Footer from "@/components/Footer";
 
 const Home = () => {
+  const account = useActiveAccount();
+
   return (
     <div className="min-h-screen bg-black text-white">
       <nav className="sticky top-0 z-50 bg-black border-b border-[#2f3336] px-4 py-3">
         <div className="max-w-[1200px] mx-auto flex justify-between items-center">
           <div className="flex items-center gap-2">
-            <h1 className="text-[30px] text-[#1d9bf0] font-bold">X.</h1>
+            <h1 className="text-[30px] text-[#1d9bf0] font-bold">Twitter</h1>
             <Twitter size={32} color="#1d9bf0" />
           </div>
-          <ConnectButton chain={chain} client={client} />
+          {account && <ConnectButton chain={chain} client={client} />}
         </div>
       </nav>
       <main className="max-w-[1200px] mx-auto mt-8 px-4">
@@ -23,7 +25,7 @@ const Home = () => {
       </main>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
